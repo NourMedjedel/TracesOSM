43e6bca2da5d507d93f3d37933dbfa41e8022f79
vdng <duong.vincent@outlook.fr>
2019-11-04 15:24:21 +0100

    update

--- post_traces.py
+++ post_traces.py
@@ -16,14 +16,15 @@ def post_osc_file(osc_file: str, model_url: str):
             # ChangeNode
             if element.tag == "node":
                 json = {
+                    "@id": "node_{}".format(element.attrib["id"]),
                     "@type": "m:{}Node".format(change.tag.title()),
                     "beginDT": timestamp,
                     "endDT": timestamp,
-                    "id": element.attrib["id"],
-                    "user": element.attrib["user"],
-                    "uid": element.attrib["uid"],
-                    "lat": element.attrib["lat"],
-                    "lon": element.attrib["lon"]
+                    "m:id": element.attrib["id"],
+                    "m:User": element.attrib["user"],
+                    "m:Uid": element.attrib["uid"],
+                    "m:Latitude": element.attrib["lat"],
+                    "m:Longitude": element.attrib["lon"]
                 }
                 r = requests.post(url=model_url, json=json)
                 print(r.status_code, r.reason)
@@ -31,12 +32,14 @@ def post_osc_file(osc_file: str, model_url: str):
             # ChangeWay
             elif element.tag == "way":
                 json = {
+                    "@id": "way_{}".format(element.attrib["id"]),
                     "@type": "m:{}Way".format(change.tag.title()),
                     "beginDT": timestamp,
                     "endDT": timestamp,
-                    "id": element.attrib["id"],
-                    "user": element.attrib["user"],
-                    "uid": element.attrib["uid"]
+                    "m:id": element.attrib["id"],
+                    "m:User": element.attrib["user"],
+                    "m:Uid": element.attrib["uid"],
+                    "m:AddNdTo": {"@id": "{}_{}".format(element.tag, element.attrib["id"])}
                 }
                 r = requests.post(url=model_url, json=json)
                 print(r.status_code, r.reason)
@@ -47,19 +50,20 @@ def post_osc_file(osc_file: str, model_url: str):
                         "@type": "m:AddNd",
                         "beginDT": timestamp,
                         "endDT": timestamp,
-                        "ref": nd.attrib["ref"]
+                        "m:Ref": nd.attrib["ref"]
                     }
                     r = requests.post(url=model_url, json=json)
-                print(r.status_code, r.reason)
+                    print(r.status_code, r.reason)
 
             elif element.tag == "relation":
                 json = {
+                    "@id": "relation_{}".format(element.attrib["id"]),
                     "@type": "m:{}Relation".format(change.tag.title()),
                     "beginDT": timestamp,
                     "endDT": timestamp,
-                    "id": element.attrib["id"],
-                    "user": element.attrib["user"],
-                    "uid": element.attrib["uid"]
+                    "m:id": element.attrib["id"],
+                    "m:User": element.attrib["user"],
+                    "m:Uid": element.attrib["uid"]
                 }
                 r = requests.post(url=model_url, json=json)
                 print(r.status_code, r.reason)
@@ -70,9 +74,10 @@ def post_osc_file(osc_file: str, model_url: str):
                         "@type": "m:AddMember",
                         "beginDT": timestamp,
                         "endDT": timestamp,
-                        "type": member.attrib["type"],
-                        "ref": member.attrib["ref"],
-                        "role": member.attrib["role"]
+                        "m:Type": member.attrib["type"],
+                        "m:Ref": member.attrib["ref"],
+                        "m:Role": member.attrib["role"],
+                        "m:AddMemberTo": {"@id": "{}_{}".format(element.tag, element.attrib["id"])}
                     }
                     r = requests.post(url=model_url, json=json)
                     print(r.status_code, r.reason)
@@ -83,8 +88,9 @@ def post_osc_file(osc_file: str, model_url: str):
                     "@type": "m:AddTag",
                     "beginDT": timestamp,
                     "endDT": timestamp,
-                    "key": tag.attrib["k"],
-                    "value": tag.attrib["v"]
+                    "m:Key": tag.attrib["k"],
+                    "m:Value": tag.attrib["v"],
+                    "m:AddTagTo": {"@id": "{}_{}".format(element.tag, element.attrib["id"])}
                 }
                 r = requests.post(url=model_url, json=json)
                 print(r.status_code, r.reason)
